0b9320c6a472a544598732f1ed80298fe94740a3
Jiseul Park <pjs2sl@snu.ac.kr>
2023-07-02 22:41:04 +0900

    Remove unnecessary fcns

--- custom_gaussian.m
+++
@@ -1,6 +0,0 @@
-function y = custom_gaussian(x, position, height, width)
-    %  gaussian(x,pos,wid) = gaussian peak centered on pos, half-width=wid
-    %  x may be scalar, vector, or matrix, pos and wid both scalar
-    %  T. C. O'Haver, 1988
-    y = height*exp(-((x - position) ./ (0.60056120439323 .* width)) .^ 2);
-end
